f6681270e0ace046ebc226139bd8b20d4bc5b92e
ishan0102 <ishan0102@gmail.com>
2020-10-24 11:11:07 -0500

    Add video recorder

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,35 @@
-import React, { Component } from 'react';
-import Button from '@material-ui/core/Button';
 import './App.css';
+import React, { Component } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { AppBar, Toolbar, Typography, Button, IconButton } from '@material-ui/core'
+import MenuIcon from '@material-ui/icons/Menu';
+import VideoRecorder from 'react-video-recorder'
 
 class App extends Component {
   render() {
+    // const { classes } = this.props;
     return (
       <div className="App">
         <header className="App-header">
-        <Button variant="contained" color="primary">
-            Hello World
-        </Button>
-          <p>
-            Edit <code>src/App.js</code> and save to reload. lolxd420
-          </p>
+            <AppBar position="fixed">
+                <Toolbar>
+                    <IconButton edge="start" className="icon" color="inherit" aria-label="menu">
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" className="typ">
+                        News
+                    </Typography>
+                    <Button color="inherit">Login</Button>
+                </Toolbar>
+            </AppBar>
         </header>
+        <body>
+            <VideoRecorder
+                onRecordingComplete={(videoBlob) => {
+                    console.log('videoBlob', videoBlob)
+                }} 
+            />
+        </body>
       </div>
     );
   }
