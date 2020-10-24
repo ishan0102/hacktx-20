00e2866f0de805a27fd0274e1ff2452ba18b08ed
Xige Michael Chen <mxchen2001@gmail.com>
2020-10-24 12:18:24 -0500

    handle video

--- client/src/components/Video.js
+++ client/src/components/Video.js
@@ -8,11 +8,25 @@ class Video extends Component {
         return (
             <VideoRecorder
                 onRecordingComplete={(videoBlob) => {
-                    MyVideoBlob = videoBlob;
+                    console.log(videoBlob);
+                    var file = new Blob(
+                        [videoBlob],
+                        {"type" : "video\/mp4"});
+                    MyVideoBlob = URL.createObjectURL(file);
+                    videoHandler();
                 }} 
             />
         );
     }
 }
+
+function videoHandler () {
+    var link = document.createElement("a"); // Or maybe get it from the current document
+    link.href = MyVideoBlob;
+    link.download = "aDefaultFileName.txt";
+    link.innerHTML = "Click here to download the file";
+    document.body.appendChild(link); // Or append it whereever you want
+}
+
 
 export default Video;
