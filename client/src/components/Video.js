bd679c067b06314ea023968b35f0c3961f1c87f3
Xige Michael Chen <mxchen2001@gmail.com>
2020-10-24 13:07:14 -0500

    blob to mp4

--- client/src/components/Video.js
+++ client/src/components/Video.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import VideoRecorder from 'react-video-recorder';
 
+
+var toWav = require('audiobuffer-to-wav');
 var MyVideoBlob;
 
 class Video extends Component {
@@ -12,8 +14,9 @@ class Video extends Component {
                     var file = new Blob(
                         [videoBlob],
                         {"type" : "video\/mp4"});
-                    MyVideoBlob = URL.createObjectURL(file);
+                    MyVideoBlob = URL.createObjectURL(videoBlob);
                     videoHandler();
+                    audioHandler(videoBlob);
                 }} 
             />
         );
@@ -23,9 +26,14 @@ class Video extends Component {
 function videoHandler () {
     var link = document.createElement("a"); // Or maybe get it from the current document
     link.href = MyVideoBlob;
-    link.download = "aDefaultFileName.txt";
+    link.download = "aDefaultFileName.mp4";
     link.innerHTML = "Click here to download the file";
+    link.click();
     document.body.appendChild(link); // Or append it whereever you want
+}
+
+function audioHandler(audioBlob) {
+
 }
 
 
